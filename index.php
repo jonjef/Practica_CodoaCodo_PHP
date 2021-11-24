736426283d6743c79187be78c7132ab3ed492c73
Jonatan Elce Fabbretti <gestoriajef@gmail.com>
2021-11-23 23:52:01 -0300

    Agrego proyecto de practica php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php 
+
+echo " Nombre: Jonatan <br/>";
+echo "Apellido: Elce Fabbretti <br/>";
+echo " Edad: 32 <br/>";
+echo "Hobbies: viajes, aviones, música <br/> ";
+echo "Editor preferido: Vs Code <br/> ";
+echo "Sistema Operativo: Windows 10 <br/>";
+
+?>
